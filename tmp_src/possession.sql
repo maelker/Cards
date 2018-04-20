a15e2ea5baf4bd5d40e91de156ba261bfd7fe3ce
Mael Kerloch <mael.kerloch@gmail.com>
2018-04-20 17:07:51 +0200

    Create possession.sql

---
+++ tmp_src/possession.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `binome36` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `binome36`;
+-- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (i686)
+--
+-- Host: db-tp.cpe.fr    Database: binome36
+-- ------------------------------------------------------
+-- Server version	5.5.59-0+deb8u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `possession`
+--
+
+DROP TABLE IF EXISTS `possession`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `possession` (
+  `idUser` int(11) DEFAULT 0,
+  `idCard` int(11) DEFAULT 0,
+  `price` int(255) DEFAULT NULL,
+  `energyCard` int(255) DEFAULT NULL,
+  `lastUsed` int(255) DEFAULT NULL,
+  PRIMARY KEY (`idUser`, `idCard`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `possession`
+--
+
+LOCK TABLES `possession` WRITE;
+/*!40000 ALTER TABLE `possession` DISABLE KEYS */;
+INSERT INTO `possession` VALUES (1,1,50,350,0),(1,2,150,50,0),(2,1,250,50,0),(2,2,350,50,0),(1,3,410,50,0),(2,4,400,50,0);
+/*!40000 ALTER TABLE `possession` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-28 11:42:55
