8526bc9362887c59471bce48b73e2a76e213c3d4
Mael Kerloch <mael.kerloch@gmail.com>
2018-04-20 17:00:50 +0200

    Create PossessionRestController.java

---
+++ java/cards/possession/controller/PossessionRestController.java
@@ -0,0 +1,93 @@
+package cards.possession.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class PossessionRestController {
+	
+	@Autowired
+	private PossessionService PossessionService;
+	
+	
+	/**
+	 * Cette fonction va faire appel au MicroService Card
+	 * afin de récupérer toutes les informations qui
+	 * corresspondent à la carte avec l'id : idcard.
+	 * @param idcard
+	 * @return [allInfoCards] Toutes les informations de la
+	 * 			carte avec l'id : idcard.
+	 */
+	@RequestMapping("/possession/idcard")
+	public String byidcard(@PathVariable int idcard){
+		return PossessionService.byidcard(idcard);
+	}
+	
+	
+	
+	/**
+	 * Cette fonction permet de récupérer toutes les cartes que possède
+	 * l'utilisateur avec l'id : iduser.
+	 * @param iduser
+	 * @return [[allInfoCards] + prices] Les Informations de toutes les
+	 * 			cartes que possède l'utilisateur avec l'id : iduser
+	 * 			avec leur prix en plus.
+	 */
+	@RequestMapping("/possession/iduser")
+	public String byiduser(@PathVariable int iduser) {
+		return PossessionService.byiduser(iduser);
+	}
+	
+	
+	/**
+	 * Cette fonction permet de récupérer toutes les cartes que possède
+	 * l'utilisateur avec l'id : iduser.
+	 * @param iduser
+	 * @return [[allInfoCards] + [allidusers] + prices] Les Informations
+	 * 			de toutes les cartes que possède l'utilisateur avec 
+	 * 			l'id : iduser avec l'id de leurs propriétaire //////////////(à voir + nom)
+	 * 			ainsi que leur prix en plus.
+	 */
+	@RequestMapping("/possession/onMarket")
+	public String onMarket(@PathVariable int iduser) {
+		return PossessionService.onMarket(iduser);
+	}
+	
+	/**
+	 * Cette fonction va changer le prix d'une carte dans la base
+	 * de donnée possbd.
+	 * @param iduser
+	 * @param idcard
+	 * @param price
+	 * @return true si tout s'est réalisée jusqu'au bout, sinon false.
+	 */
+	@RequestMapping("/possession/setPrice")
+	public void setPrice(@PathVariable int iduser, @PathVariable int idcard,
+						 @PathVariable int price) {
+		PossessionService.setPrice(iduser, idcard, price);
+	}
+	
+	/**
+	 * Cette fonction va faire appel au MicroService User afin de savoir
+	 * si l'utilisateur à assez d'argent, si c'est bon alors il va changer
+	 * l'id de la carte dans la base de donnée possbd pour changer
+	 * l'utilisateur puis demander au MS User de faire le virement.
+	 * @param iduser
+	 * @param iduserCard
+	 * @param idcard
+	 * @param price
+	 * @return true si la transaction s'est réalisée jusqu'au bout, sinon false.
+	 */
+	@RequestMapping("/possession/buyCard")
+	public void buyCard(@PathVariable int iduser, @PathVariable int iduserCard,
+						@PathVariable int idcard, @PathVariable int price) {
+		PossessionService.buyCard(iduser, iduserCard, idcard, price);
+	}
+	
+	
+}
